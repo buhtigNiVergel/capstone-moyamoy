a4660ecc90fc48447d28abf6185f99ff9ca3494b
yomsk <moyamoy.vergelbscs2023@gmail.com>
2026-08-08 12:46:51 +0800

    checkout page move into react

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,10 +1,20 @@
-import { HomePage } from './pages/HomePage'
+import { Routes, Route } from 'react-router';
+import { HomePage } from './pages/HomePage';
+import { CheckoutPage } from './pages/CheckouPage';
 
 import './App.css'
 function App() {
 
   return (
-    <HomePage />
+    <Routes>
+      <Route 
+        path="/"
+        element={<HomePage />} />
+
+      <Route 
+        path="checkout"
+        element={<CheckoutPage />} />
+    </Routes>
   )
 }
 
