2c8890bd7ceeb45f423eb16c46ba018a43c64521
yomsk <moyamoy.vergelbscs2023@gmail.com>
2026-08-08 12:52:43 +0800

    current

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -8,8 +8,7 @@ function App() {
   return (
     <Routes>
       <Route 
-        path="/"
-        element={<HomePage />} />
+        index element={<HomePage />} />
 
       <Route 
         path="checkout"
