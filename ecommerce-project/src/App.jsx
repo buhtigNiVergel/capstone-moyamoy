7fc2b15e56cbab206f22ae365c38287008baea6e
yomsk <moyamoy.vergelbscs2023@gmail.com>
2026-08-09 17:04:40 +0800

    current god

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,4 +1,6 @@
+import axios from 'axios'
 import { Routes, Route } from 'react-router';
+import { useState, useEffect } from 'react';
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { Orders } from './pages/orders';
@@ -8,14 +10,24 @@ import { NotFoundPage } from './pages/NotFoundPage'
 import './App.css'
 function App() {
 
+  const [cart, setCart] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/cart-items')
+      .then((response) => {
+    
+        setCart(response.data);
+    })
+  }, [])
+
   return (
     <Routes>
       <Route 
-        index element={<HomePage />} />
+        index element={<HomePage cart = {cart}/>} />
 
       <Route 
         path="Checkout"
-        element={<CheckoutPage />} />
+        element={<CheckoutPage cart = {cart}/>} />
 
       <Route 
         path="Orders"
