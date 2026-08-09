4f40bfb7fd6e39e4fae0bee92da6b18c56b8f414
yomsk <moyamoy.vergelbscs2023@gmail.com>
2026-08-09 11:16:24 +0800

    tracking page added

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { Orders } from './pages/orders';
+import { Tracking } from './pages/Tracking';
  
 import './App.css'
 function App() {
@@ -12,12 +13,16 @@ function App() {
         index element={<HomePage />} />
 
       <Route 
-        path="checkout"
+        path="Checkout"
         element={<CheckoutPage />} />
 
       <Route 
         path="Orders"
         element={<Orders />} />
+
+      <Route 
+        path="Tracking"
+        element={<Tracking />}/>
     </Routes>
   )
 }
