cedaaab2e2d741c03668847aa7883375abc7812f
yomsk <moyamoy.vergelbscs2023@gmail.com>
2026-08-11 02:25:43 +0800

    8e exercise finish

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -18,7 +18,8 @@ export function CartItemDetails({ cartItem, loadCart }) {
         </div>
         <div className="product-quantity">
           <span>
-            Quantity: <span className="quantity-label">{cartItem.quantity}</span>
+             Quantity: <input type="text" className="quantity-textbox" />
+                    <span className="quantity-label">{cartItem.quantity}</span>
           </span>
           <span className="update-quantity-link link-primary">Update</span>
           <span className="delete-quantity-link link-primary"
