543a9e6488dc5bc10d011b76313e12951974013d
yomsk <moyamoy.vergelbscs2023@gmail.com>
2026-08-10 16:05:59 +0800

    7b exercise finish

--- ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,6 +1,6 @@
 import dayjs from "dayjs";
-import { formatMoney } from "../../utils/money";
 import { DeliveryOption } from "./DeliveryOptions";
+import { CartItemDetails } from "./CartItemDetails";
 
 export function OrderSummary( {cart, deliveryOptions} ) {
   return (
@@ -23,28 +23,7 @@ export function OrderSummary( {cart, deliveryOptions} ) {
               </div>
 
               <div className="cart-item-details-grid">
-                <img className="product-image" src={cartItem.product.image} />
-
-                <div className="cart-item-details">
-                  <div className="product-name">{cartItem.product.name}</div>
-                  <div className="product-price">
-                    {formatMoney(cartItem.product.priceScents)}
-                  </div>
-                  <div className="product-quantity">
-                    <span>
-                      Quantity:{" "}
-                      <span className="quantity-label">
-                        {cartItem.quantity}
-                      </span>
-                    </span>
-                    <span className="update-quantity-link link-primary">
-                      Update
-                    </span>
-                    <span className="delete-quantity-link link-primary">
-                      Delete
-                    </span>
-                  </div>
-                </div>
+                <CartItemDetails cartItem={cartItem} />
 
                 <DeliveryOption cartItem={cartItem} deliveryOptions={deliveryOptions} />
               </div>
