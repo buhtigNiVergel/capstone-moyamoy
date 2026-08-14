ad44b73701a202d1e5cb7a9de3ef442a83c2016d
yomsk <moyamoy.vergelbscs2023@gmail.com>
2026-08-14 13:20:53 +0800

    finish 9c exercise

--- ecommerce-project/src/pages/home/Product.jsx
+++ ecommerce-project/src/pages/home/Product.jsx
@@ -54,6 +54,7 @@ export function Product({ product, loadCart }) {
         <select
           value={quantity}
           onChange={selectQuantity}
+          data-testid="product-quantity-selector"
         >
           <option value="1">1</option>
           <option value="2">2</option>
--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -69,5 +69,12 @@ describe("Product component", () => {
         }
     );
     expect(loadCart).toHaveBeenCalled();
-  })
+  });
+
+  it('selects a quantity', () => {
+    render(<Product product={product} loadCart={loadCart} />);
+
+    const quantitySelector = screen.getByTestId('product-quantity-selector');
+    expect(quantitySelector).toHaveValue('1');
+  });
 });
